96e21d557f611ee7b18ffc24b8890a7ced4f6aaf
Dov Rine <dov@techimpact.org>
2021-09-08 16:34:02 -0700

    implement initial endpoint for listing products

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,12 @@
-function makeServer() {}
+import express from 'express'
+
+function makeServer() {
+    const app = express()
+
+    app.get('/api/products', (req, res) => {
+        res.status(200).json([])
+    })
+    return app
+}
 export { makeServer };
 export default makeServer;
--- backend/src/server.test.js
+++ backend/src/server.test.js
@@ -1,7 +1,23 @@
+import supertest from 'supertest'
 import makeServer from './server.js'
+const app = makeServer()
 
 describe('server', () => {
     it('exists', () => {
-        expect(typeof makeServer).toBe('function')
+        expect(typeof app).toBe('function')
     })
+    it("has an endpoint for listing products", async () => {
+        await supertest(app).get("/api/products")
+          .expect(200)
+          .then((response) => {
+            // Check type and length
+            expect(Array.isArray(response.body)).toBeTruthy();
+            expect(response.body.length).toEqual(0);
+          });
+      });
+    it.todo('has an endpoint for listing products')
+    it.todo('has an endpoint for adding a product')
+    it.todo('has an endpoint for editing a product')
+    it.todo('has an endpoint for deleting a product')
+    it.todo('has an endpoint for processing payments')
 })
