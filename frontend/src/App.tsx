abd915c08b665e6a478dc7226759d9201df29770
Jose Argueta <jajose780@gmail.com>
2021-09-09 16:04:41 -0700

    minor changes to latout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,26 @@
-import './App.scss'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link,
-} from 'react-router-dom'
-import TermsAndConditions from './components/TermsAndConditions'
-import Header from './components/Header'
+import "./App.scss"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import TermsAndConditions from "./components/TermsAndConditions"
+import Header from "./components/Header"
 function App() {
   return (
     <Router>
-      <div className='App'>
-        <div className='container'>
+      <div className="App">
+        <div className="container">
           <Header />
-          <div className='main'>
+          <div className="main">
             <div>
               <Switch>
-                <Route path='/TermsAndConditions'>
+                <Route path="/TermsAndConditions">
                   <TermsAndConditions />
                 </Route>
-                <Route path='/'>
+                <Route path="/">
                   <Shop />
                 </Route>
               </Switch>
             </div>
           </div>
-          <div className='footer'></div>
+          <div className="footer"></div>
         </div>
       </div>
     </Router>
